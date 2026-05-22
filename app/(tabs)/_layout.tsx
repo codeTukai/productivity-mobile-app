fa0fa2376eebff6f6606c545ce4c393813de3e14
codeTukai <tukaighosh753@gmail.com>
2026-05-22 16:58:04 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added babel-preset-expo and nativewind to package.json
    - Included prettier-plugin-tailwindcss and tailwindcss for styling
    - Created tailwind.config.js for Nativewind setup
    - Updated tsconfig.json for consistency

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,23 @@
-
-import React from 'react';
-import { Stack } from 'expo-router';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return <Stack />;
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
